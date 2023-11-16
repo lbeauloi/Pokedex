d3060cfb25c699ee9010a7de7725ea4328639f51
Taweria <elodieali.pro@gmail.com>
2023-11-16 14:37:21 +0100

    check login and log out

---
+++ check_login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+function checkLogin(){
+
+    if (!isset($_SESSION['username'])) {
+        header('Location: login.php');
+        exit();
+    }
+
+}
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = sha1($_POST['password']);
+
+    $bdd = connectDB();
+    $req = $bdd->prepare('SELECT * FROM user WHERE username = :username AND password = :password');
+    $req->execute(array(
+        'username' => $username,
+        'password' => $password
+    ));
+
+    $data = $req->fetch();
+
+}
+
+if ($data) {
+    $_SESSION['username'] = $username;
+    header('Location: index.php');
+    exit();
+} 
+
+?>
