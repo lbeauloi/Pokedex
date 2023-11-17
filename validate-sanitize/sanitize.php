a0908754d05a6407731cda5b306451c60398d33d
Lidwine Carême <lidwine.careme@gmail.com>
2023-11-17 10:17:53 +0100

    validation

--- validate-sanitize/sanitize.php
+++ validate-sanitize/sanitize.php
@@ -3,27 +3,53 @@ require('./connect.php');
 global $bdd;
 
 
-function getErrors()
-{
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $emailErr = empty($_POST["email"]) ? "* Email is required" : (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? "*Invalid email" : "");
-        $passwordErr =  empty($_POST["password"]) ? "* Password is required" : "";
-        $passwordConfirmErr = empty($_POST["password"]) ? "* Password is required" : "";
-        return array(
-            "email" => $emailErr,
-            "password" => $passwordErr,
-            "password2" => $passwordConfirmErr
 
-        );
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirmpassword = $_POST['confirmpassword'];
+    $passwordlen = strlen($password);
+    $min = 7;
+    $emailErr = empty($username) ? "* Email is required" : (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? "*Invalid email" : "");
+    $passwordErr =  empty($password) ? "* Password is required" : ($passwordlen < $min ? "*Password should have min 7 characters" : "");
+    $passwordConfirmErr = empty($confirmpassword) ? "* Password is required" : ($password != $confirmpassword ? "*password doesn't match" : "");
+    return array(
+        "email" => $emailErr,
+        "password" => $passwordErr,
+        "password2" => $passwordConfirmErr
+
+    );
+
+    if (!empty($_POST['username']) && !empty($_POST['password'])) {
+        if ($password == $confirmpassword) {
+            //pour crypter mot de passe avant de le stocker
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $add = "INSERT INTO users (username, Password) VALUES ('.$username.', '.$hash.')";
+            $nb = $bdd->prepare($add);
+            // header('Location: login.php');
+            // exit;
+
+        }
     }
 }
-    
-    // $add = 'INSERT INTO users (username,password ) VALUES("' . $_POST['email'] . '","' . $_POST['password'] . '")';
-    // $nb = $bdd->exec($add);
-    // header('Location: login.php');
-    // exit;
+
+
 
 
 
 // trouver ce qui egalise les 2 passwords via une methode, le stocker dans une fonction qu'on appellera plus haut dans $passwordErr
 //dans cette fonction il faudra return true si egale et false si pas égal 
+
+
+
+//pour crypter mot de passe avant de le stocker
+
+
+//Vérifie que le hachage fourni correspond bien au mot de passe fourni. A  mon avis plus utile pour le login
+if (password_verify($password, $hash)) {
+    echo 'Le mot de passe est valide !';
+} else {
+    echo 'Le mot de passe est invalide.';
+}
