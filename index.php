6475ce31a4fda0e03185f27603c9ec1293663574
Lucas Beauloi <l.beauloi@gmail.com>
2023-11-20 10:07:58 +0100

    type style

--- index.php
+++ index.php
@@ -39,32 +39,52 @@ displayPokemons();
 
 function displayPokemons(){
    global $bdd;
-    $query = 'SELECT pokemon.pokemonID ,pokemon.name, pokemon.number, pokemon.picture, GROUP_CONCAT(types.name) AS typeNames
-              FROM pokemon
-              JOIN pokemontype ON pokemon.pokemonID = pokemontype.pokemonID
-              JOIN types ON pokemontype.typeID = types.typeID
-              GROUP BY pokemon.pokemonID';
+    // $query = 'SELECT pokemon.pokemonID ,pokemon.name, pokemon.number, pokemon.picture, GROUP_CONCAT(types.name) AS typeNames
+    //           FROM pokemon
+    //           JOIN pokemontype ON pokemon.pokemonID = pokemontype.pokemonID
+    //           JOIN types ON pokemontype.typeID = types.typeID
+    //           GROUP BY pokemon.pokemonID';
+    $query = 'SELECT pokemon.pokemonID, pokemon.name, pokemon.number, pokemon.picture, GROUP_CONCAT(types.name) AS typeNames
+          FROM pokemon
+          JOIN pokemontype ON pokemon.pokemonID = pokemontype.pokemonID
+          JOIN types ON pokemontype.typeID = types.typeID
+          GROUP BY pokemon.pokemonID';
 
-    foreach ($bdd->query($query) as $row) {
-        // Pour chaque enregistrement, afficher une entrée de liste
-        $checked = evaluateFavorite($row['pokemonID']);
-        echo
-            '<div class="container">
-                <img class="imagePokemon" src="' . $row["picture"] . '" alt="image du pokemon">
-                <p class="number">' . $row["number"] . '</p>
-                <p class="name"><a class="nameLink" href="detail.php?id=' . $row["pokemonID"] . '">' . $row["name"] . '</a></p>
-                <div class="types"><p class="types">' . str_replace(",", '</p> <p class="types">', $row["typeNames"])  .  '</p></div>
-                
 
-                <div class="heart">
-                     <form action="favoritesManager.php" method="GET">
-                     <input type="hidden" name="pokemonId" value="' . $row['pokemonID'] . '"/>
-                     <input onChange="submit()" type="checkbox" id="heart' . $row['pokemonID'] . '" '.$checked.'/>
-                     <label for="heart' . $row['pokemonID'] . '"></label>
-                     </form>
-                 </div>
-            </div>';
+foreach ($bdd->query($query) as $row) {
+    $checked = evaluateFavorite($row['pokemonID']);
+    
+    $types = explode(',', $row['typeNames']); // Sépare les types par une virgule
+    $typeClasses = array_map('strtolower', $types); // Convertit les types en minuscules
+
+    echo
+        '<div class="container">';
+    
+    // Afficher l'image, le numéro et le nom du Pokémon
+    echo '<img class="imagePokemon" src="' . $row["picture"] . '" alt="image du pokemon">';
+    echo '<p class="number">' . $row["number"] . '</p>';
+    echo '<p class="name"><a class="nameLink" href="detail.php?id=' . $row["pokemonID"] . '">' . $row["name"] . '</a></p>';
+
+    // Afficher les types avec les styles distincts pour chaque type
+    echo '<div class="types">';
+    foreach ($types as $type) {
+        echo '<p class="' . strtolower($type) . '">' . $type . '</p>';
     }
+    echo '</div>';
+
+    // Afficher la partie pour le cœur (favori)
+    echo '<div class="heart">';
+    echo '<form action="favoritesManager.php" method="GET">';
+    echo '<input type="hidden" name="pokemonId" value="' . $row['pokemonID'] . '"/>';
+    echo '<input onChange="submit()" type="checkbox" id="heart' . $row['pokemonID'] . '" '.$checked.'/>';
+    echo '<label for="heart' . $row['pokemonID'] . '"></label>';
+    echo '</form>';
+    echo '</div>';
+
+    echo '</div>';
+}
+
+
 
 }
 function evaluateFavorite($pokemonId):string{
